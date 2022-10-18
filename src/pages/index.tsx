0ffacbada8d97c7956040f92d71116dcc5527321
Tatsuya1117 <othe.world.pc@gmail.com>
2022-10-18 14:20:35 +0900

    testCommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,9 @@ export default function Home({ articles }: Props) {
   return (
     <>
       <SEO
-        title={"GOOD-DAY(グッディ)|岡山県倉敷市の個室シェアサロン"}
+        title={"GOOD-DAY(グッディ)|岡山県の個室シェアサロン"}
         description={
-          "岡山県倉敷市にあるBeauty Hair Salon GOOD-DAY(グッディ)では、独立したい美容師さんを応援しています。全ての美容師に夢を与えられるようなシェアサロンを目指しています。"
+          "Beauty Hair Salon GOOD-DAY(グッディ)では、独立したい美容師さんを応援しています。全ての美容師に夢を与えられるようなシェアサロンを目指しています。"
         }
         keyword={
           "岡山県,倉敷市,美容室,シェアサロン,GOOD-DAY,グッディ,独立,独立支援"
