1681241b61206e658de4b9043cf7df74d0aaaa9b
Tatsuya1117 <othe.world.pc@gmail.com>
2023-03-29 11:38:14 +0900

    yukaのMenuを修正

--- src/components/menu/Yuka.tsx
+++ src/components/menu/Yuka.tsx
@@ -6,6 +6,64 @@ import styles from "../../styles/StylistMenu/StylistMenu.module.scss";
 export default function Yuka() {
   return (
     <div className={styles.menu}>
+      <h2 className={styles.menuTitle}>おすすめMenu</h2>
+      <table className={styles.menuArea}>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>カット + カラー</th>
+            <td className={styles.menuTableTd}>
+              カットとカラーのセットMenuです。
+            </td>
+            <td className={styles.menuTableTd}>¥11,660〜</td>
+            <td className={styles.menuTableTd}>150分</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>カット + トリートメント</th>
+            <td className={styles.menuTableTd}>
+              カットとトリートメントのセットMenuです。
+            </td>
+            <td className={styles.menuTableTd}>¥7,260</td>
+            <td className={styles.menuTableTd}>120分</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>カラー + トリートメント</th>
+            <td className={styles.menuTableTd}>
+              カラーとトリートメントのセットMenuです。
+            </td>
+            <td className={styles.menuTableTd}>¥11,660〜</td>
+            <td className={styles.menuTableTd}>120分</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>
+              白髪染め(根本リタッチ) + カット
+            </th>
+            <td className={styles.menuTableTd}>
+              白髪染めのリタッチとカットのセットMenuです。
+            </td>
+            <td className={styles.menuTableTd}>¥10,560〜</td>
+            <td className={styles.menuTableTd}>90分</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTablLast}>
+            <th className={styles.menuTableTh}>
+              白髪染め(根本リタッチ) + トリートメント
+            </th>
+            <td className={styles.menuTableTd}>
+              白髪染めのリタッチとトリートメントのセットMenuです。
+            </td>
+            <td className={styles.menuTableTd}>¥10,560〜</td>
+            <td className={styles.menuTableTd}>90分</td>
+          </tr>
+        </tbody>
+      </table>
+
       <h2 className={styles.menuTitle}>カット</h2>
       <p className={styles.menuDetail}>(シャンプー・ブロー込)</p>
       <table className={styles.menuArea}>
@@ -19,34 +77,34 @@ export default function Yuka() {
         </tbody>
         <tbody>
           <tr>
-            <th className={styles.menuTableTh}>中高生</th>
+            <th className={styles.menuTableTh}>中高生カット</th>
             <td className={styles.menuTableTd}>(シャンプー込)</td>
-            <td className={styles.menuTableTd}>¥3,630</td>
+            <td className={styles.menuTableTd}>¥3,740</td>
             <td className={styles.menuTableTd}>60分</td>
           </tr>
         </tbody>
-        <tbody>
+        {/* <tbody>
           <tr className={styles.menuTablLast}>
             <th className={styles.menuTableTh}>小学生</th>
             <td className={styles.menuTableTd}>(シャンプーなし)</td>
             <td className={styles.menuTableTd}>¥2,750</td>
             <td className={styles.menuTableTd}>60分</td>
           </tr>
-        </tbody>
-        <tbody>
+        </tbody> */}
+        {/* <tbody>
           <tr className={styles.menuTablLast}>
             <th className={styles.menuTableTh}>小学生未満カット</th>
             <td className={styles.menuTableTd}>(シャンプーなし)</td>
             <td className={styles.menuTableTd}>¥2,200</td>
             <td className={styles.menuTableTd}>60分</td>
           </tr>
-        </tbody>
+        </tbody> */}
         <tbody>
           <tr className={styles.menuTablLast}>
             <th className={styles.menuTableTh}>前髪カット</th>
             <td className={styles.menuTableTd}>(シャンプーなし)</td>
-            <td className={styles.menuTableTd}>¥550</td>
-            <td className={styles.menuTableTd}>15分</td>
+            <td className={styles.menuTableTd}>¥770</td>
+            <td className={styles.menuTableTd}>20分</td>
           </tr>
         </tbody>
         <tbody>
@@ -55,7 +113,7 @@ export default function Yuka() {
             <td className={styles.menuTableTd}>
               (シャンプーなし, 襟足や刈り上げなど)
             </td>
-            <td className={styles.menuTableTd}>¥2,200</td>
+            <td className={styles.menuTableTd}>¥3,300</td>
             <td className={styles.menuTableTd}>40分</td>
           </tr>
         </tbody>
@@ -70,7 +128,7 @@ export default function Yuka() {
           <tr className={styles.menuTableTr}>
             <th className={styles.menuTableTh}>リタッチ</th>
             <td className={styles.menuTableTd}>前回来店から30日以内</td>
-            <td className={styles.menuTableTd}>¥3,850</td>
+            <td className={styles.menuTableTd}>¥4,180</td>
             <td className={styles.menuTableTd}>90分〜</td>
           </tr>
         </tbody>
@@ -78,7 +136,7 @@ export default function Yuka() {
           <tr className={styles.menuTableTr}>
             <th className={styles.menuTableTh}>リタッチ</th>
             <td className={styles.menuTableTd}>前回来店から60日以内</td>
-            <td className={styles.menuTableTd}>¥4,400</td>
+            <td className={styles.menuTableTd}>¥4,730</td>
             <td className={styles.menuTableTd}>90分〜</td>
           </tr>
         </tbody>
@@ -86,15 +144,15 @@ export default function Yuka() {
           <tr className={styles.menuTableTr}>
             <th className={styles.menuTableTh}>リタッチ</th>
             <td className={styles.menuTableTd}>3cmまで</td>
-            <td className={styles.menuTableTd}>¥5,500</td>
+            <td className={styles.menuTableTd}>¥5,830</td>
             <td className={styles.menuTableTd}>90分〜</td>
           </tr>
         </tbody>
         <tbody>
           <tr className={styles.menuTableTr}>
-            <th className={styles.menuTableTh}>カラー</th>
+            <th className={styles.menuTableTh}>カラー全体</th>
             <td className={styles.menuTableTd}>&nbsp;</td>
-            <td className={styles.menuTableTd}>¥6,600〜</td>
+            <td className={styles.menuTableTd}>¥6,930〜</td>
             <td className={styles.menuTableTd}>90分〜</td>
           </tr>
         </tbody>
@@ -102,29 +160,32 @@ export default function Yuka() {
           <tr className={styles.menuTablLast}>
             <th className={styles.menuTableTh}>ブリーチ</th>
             <td className={styles.menuTableTd}>&nbsp;</td>
-            <td className={styles.menuTableTd}>¥8,800〜</td>
+            <td className={styles.menuTableTd}>¥9,130〜</td>
             <td className={styles.menuTableTd}>180分〜</td>
           </tr>
         </tbody>
         <tbody>
           <tr className={styles.menuTablLast}>
-            <th className={styles.menuTableTh}>部分カラー</th>
-            <td className={styles.menuTableTd}>&nbsp;</td>
-            <td className={styles.menuTableTd}>¥3,300〜</td>
-            <td className={styles.menuTableTd}>120分〜</td>
+            <th className={styles.menuTableTh}>インナーブリーチ</th>
+            <td className={styles.menuTableTd}>
+              (デザインにより料金・時間変動あり)
+            </td>
+            <td className={styles.menuTableTd}>¥5,830〜</td>
+            <td className={styles.menuTableTd}>180分〜</td>
           </tr>
         </tbody>
         <tbody>
           <tr className={styles.menuTablLast}>
-            <th className={styles.menuTableTh}>特殊カラー</th>
+            <th className={styles.menuTableTh}>ハイライト、グラデーション</th>
             <td className={styles.menuTableTd}>
-              インナー、ハイライト、グラデーションetc…
+              (デザインにより料金・時間変動あり)
             </td>
-            <td className={styles.menuTableTd}>¥5,500〜</td>
+            <td className={styles.menuTableTd}>¥7,700〜</td>
             <td className={styles.menuTableTd}>180分〜</td>
           </tr>
         </tbody>
       </table>
+
       <h2 className={styles.menuTitle}>パーマ(ブロー込)</h2>
       <p className={styles.menuDetail}>☆長さ内容によって＋料金あります</p>
       <p className={styles.menuDetail}>☆カット別・SB込</p>
@@ -135,7 +196,7 @@ export default function Yuka() {
         <tbody>
           <tr className={styles.menuTableTr}>
             <th className={styles.menuTableTh}>パーマ</th>
-            <td className={styles.menuTableTd}>&amp;</td>
+            <td className={styles.menuTableTd}>&nbsp;</td>
             <td className={styles.menuTableTd}>¥6,600〜</td>
             <td className={styles.menuTableTd}>120〜180分</td>
           </tr>
@@ -158,57 +219,67 @@ export default function Yuka() {
       <table className={styles.menuArea}>
         <tbody>
           <tr className={styles.menuTableTr}>
+            <th className={styles.menuTableTh}>縮毛矯正</th>
+            <td className={styles.menuTableTd}>&nbsp;</td>
+            <td className={styles.menuTableTd}>¥13,750〜</td>
+            <td className={styles.menuTableTd}>150分〜240分</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTableTr}>
             <th className={styles.menuTableTh}>リタッチ</th>
             <td className={styles.menuTableTd}>(4ヶ月以内)</td>
-            <td className={styles.menuTableTd}>¥11,000〜</td>
+            <td className={styles.menuTableTd}>¥11,550〜</td>
             <td className={styles.menuTableTd}>120分〜240分</td>
           </tr>
         </tbody>
         <tbody>
           <tr className={styles.menuTableTr}>
-            <th className={styles.menuTableTh}>ストレート全体</th>
+            <th className={styles.menuTableTh}>前髪縮毛矯正</th>
             <td className={styles.menuTableTd}>詳細</td>
-            <td className={styles.menuTableTd}>¥13,200〜</td>
-            <td className={styles.menuTableTd}>150分〜240分</td>
-          </tr>
-        </tbody>
-        <tbody>
-          <tr className={styles.menuTableTr}>
-            <th className={styles.menuTableTh}>前髪ストレート</th>
-            <td className={styles.menuTableTd}>詳細</td>
-            <td className={styles.menuTableTd}>¥3,300〜</td>
+            <td className={styles.menuTableTd}>¥5,500〜</td>
             <td className={styles.menuTableTd}>90分〜120分</td>
           </tr>
         </tbody>
         <tbody>
           <tr className={styles.menuTablLast}>
-            <th className={styles.menuTableTh}>ストレートハーフ</th>
+            <th className={styles.menuTableTh}>部分縮毛矯正</th>
             <td className={styles.menuTableTd}>&nbsp;</td>
-            <td className={styles.menuTableTd}>¥6,600〜</td>
+            <td className={styles.menuTableTd}>¥7,700〜</td>
             <td className={styles.menuTableTd}>150分〜240分</td>
           </tr>
         </tbody>
       </table>
+
       <h2 className={styles.menuTitle}>トリートメント</h2>
       <table className={styles.menuArea}>
         <tbody>
           <tr className={styles.menuTableTr}>
-            <th className={styles.menuTableTh}>スタンダード</th>
-            <td className={styles.menuTableTd}>&nbsp;</td>
-            <td className={styles.menuTableTd}>¥2,200</td>
-            <td className={styles.menuTableTd}>150分</td>
+            <th className={styles.menuTableTh}>トリキュアクイック</th>
+            <td className={styles.menuTableTd}>シャンプー・ブロー別</td>
+            <td className={styles.menuTableTd}>¥2,530</td>
+            <td className={styles.menuTableTd}>20分</td>
           </tr>
         </tbody>
         <tbody>
           <tr className={styles.menuTablLast}>
-            <th className={styles.menuTableTh}>プレミアム</th>
-            <td className={styles.menuTableTd}>&nbsp;</td>
-            <td className={styles.menuTableTd}>¥6,600〜</td>
-            <td className={styles.menuTableTd}>20分</td>
+            <th className={styles.menuTableTh}>トリキュアトリートメント</th>
+            <td className={styles.menuTableTd}>シャンプー・ブロー別</td>
+            <td className={styles.menuTableTd}>¥3,300</td>
+            <td className={styles.menuTableTd}>30分</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr className={styles.menuTablLast}>
+            <th className={styles.menuTableTh}>高保湿トリートメント</th>
+            <td className={styles.menuTableTd}>シャンプー・ブロー別</td>
+            <td className={styles.menuTableTd}>¥3,850</td>
+            <td className={styles.menuTableTd}>30分</td>
           </tr>
         </tbody>
       </table>
-      <h2 className={styles.menuTitle}>セットメニュー</h2>
+
+      {/* <h2 className={styles.menuTitle}>セットメニュー</h2>
       <table className={styles.menuArea}>
         <tbody>
           <tr className={styles.menuTableTr}>
@@ -250,13 +321,14 @@ export default function Yuka() {
             <td className={styles.menuTableTd}>90分</td>
           </tr>
         </tbody>
-      </table>
+      </table> */}
+
       <h2 className={styles.menuTitle}>その他</h2>
       <table className={styles.menuArea}>
         <tbody>
           <tr className={styles.menuTableTr}>
             <th className={styles.menuTableTh}>シャンプー</th>
-            <td className={styles.menuTableTd}>&npsp;</td>
+            <td className={styles.menuTableTd}>&nbsp;</td>
             <td className={styles.menuTableTd}>¥1,100</td>
             <td className={styles.menuTableTd}>15分</td>
           </tr>
@@ -264,7 +336,7 @@ export default function Yuka() {
         <tbody>
           <tr className={styles.menuTableTr}>
             <th className={styles.menuTableTh}>炭酸スパシャンプー</th>
-            <td className={styles.menuTableTd}>&npsp;</td>
+            <td className={styles.menuTableTd}>&nbsp;</td>
             <td className={styles.menuTableTd}>¥1,650</td>
             <td className={styles.menuTableTd}>20分</td>
           </tr>
@@ -272,7 +344,7 @@ export default function Yuka() {
         <tbody>
           <tr className={styles.menuTableTr}>
             <th className={styles.menuTableTh}>ブロー</th>
-            <td className={styles.menuTableTd}>&npsp;</td>
+            <td className={styles.menuTableTd}>&nbsp;</td>
             <td className={styles.menuTableTd}>¥1,100</td>
             <td className={styles.menuTableTd}>30分</td>
           </tr>
@@ -290,7 +362,7 @@ export default function Yuka() {
         <tbody>
           <tr className={styles.menuTablLast}>
             <th className={styles.menuTableTh}>アップ</th>
-            <td className={styles.menuTableTd}>&npsp;</td>
+            <td className={styles.menuTableTd}>&nbsp;</td>
             <td className={styles.menuTableTd}>¥4,400〜¥5,500</td>
             <td className={styles.menuTableTd}>60分〜</td>
           </tr>
